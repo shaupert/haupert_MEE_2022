0c9d790b214ad474702139dc269f09c6f2d56f0e
Sylvain Haupert <sylvain.haupert@mnhn.fr>
2021-12-14 16:44:35 +0100

    Update toolbox_propa.R

--- scripts/toolbox/toolbox_propa.R
+++ scripts/toolbox/toolbox_propa.R
@@ -1,5 +1,161 @@
-##### CALL functions from MY TOOLBOX
-source("./wav2dBSPL.R", chdir=T)
+###**********************************************************************###
+# Functions to convert wav file into Sound Pressure Level dB SPL
+# Functions to calculate outdoor propagation in natural habitat
+# Functions to estimate the detection or active distance
+#
+# Author : Sylvain HAUPERT, MNHN, sylvain.haupert@mnhn.fr
+# Date : 14/12/2021
+# Licence : MIT
+####**********************************************************************###
+
+if (!require("signal")) install.packages("signal")
+library(signal)
+
+# Constant : minimum value possible
+MIN = 2.2250738585072014e-308
+
+#####################
+dB2amplitude <- function (db)
+{
+  return (10**(db/20))
+}
+
+dB2power <- function (db)
+{
+  return (10**(db/10))
+}
+
+amplitude2dB <- function (s)
+{
+  return (20*log10(s))
+}
+
+power2dB <- function (s)
+{
+  return (10*log10(s))
+}
+
+####################
+
+##### wav2volt
+wav2volt <- function (wave, bit=16, Vadc=2) 
+{ 
+  # convert in Volt. Vadc is 2Vpp [peak to peak] for the SM4
+  volt = (wave/(2^(bit))) * Vadc
+  return (volt)
+}
+
+#####  volt2pressure
+volt2pressure <- function (volt, gain, sensitivity=-35) 
+{  
+  #volt amplitude to instantaneous sound pressure (Pa)
+  coef = 1/10^(sensitivity/20) 
+  p = volt * coef / 10^(gain/20)
+  
+  return(p)
+}
+
+#####  wav2pressure
+wav2pressure <- function (wave, gain, sensitivity=-35, bit=16, Vadc=2) 
+{
+  # Vadc => 2Vpp [peak to peak] for the SM4
+  p = volt2pressure(wav2volt(wave, bit, Vadc), gain, sensitivity) 
+  
+  return (p)
+}
+
+###################################"
+
+#####  pressure2dBSPL
+pressure2dBSPL <- function(p, pRef=20e-6)
+{
+  # if p <=0 set to MIN
+  p[p==0] = MIN
+  # Take the log of the ratio pressure/pRef
+  L = 20*log10(p/pRef) 
+  return (L)
+}
+
+#####  dBSPL2pressure
+dBSPL2pressure <- function(L, pRef=20e-6)
+{
+  # dB SPL to pressure
+  p = 10**(L/20)*pRef
+  return (p)
+}                
+
+#####  wav2dBSPL
+wav2dBSPL <- function (wave, gain, sensitivity=-35, bit=16, Vadc=2, pRef=20e-6) 
+{
+  p = wav2pressure(wave, gain, sensitivity, bit, Vadc) 
+  L = pressure2dBSPL(p, pRef)
+  return (L)
+}
+
+#####  psd2dBSPL
+psd2dBSPL <- function (P, gain, sensitivity=-35, bit=16, Vadc=2, pRef=20e-6) 
+{
+  # convert power (energy) to amplitude
+  s = sqrt(P)
+  # convert amplitude to dB sPL
+  L = wav2dBSPL(s, gain, sensitivity, bit, Vadc, pRef)
+  return (L)
+}
+
+########################
+
+#####  wav2Leq
+wav2leq <- function (wave, f, gain, dt=1, sensitivity=-35, bit=16, Vadc=2, pRef= 20e-6) 
+{  
+  # convert into pressure
+  p = wav2pressure(wave, gain, sensitivity, bit, Vadc) 
+  
+  # p to Leq (Equivalent Continuous Sound level)
+  Leq = pressure2leq(p, f, dt, pRef) 
+  
+  return(Leq)
+}
+
+#####  pressure2Leq
+pressure2leq <- function (p, f, dt=1, pRef = 20e-6) 
+{  
+  # wav to RMS
+  dN = dt*f # integration period in number of points
+  N_RMS = floor(length(p)/dN)
+  
+  p_RMS = matrix(NA,nrow=1,ncol=N_RMS)
+  
+  for (ii in 1:N_RMS)
+  {
+    p_mean = mean(p[(1+(ii-1)*dN):(ii*dN)]^2)
+    p_RMS[ii] = sqrt(p_mean)
+  }
+  
+  # if p_RMS ==0 set to MIN
+  p_RMS[p_RMS==0] = MIN
+  
+  # RMS to Leq (Equivalent Continuous Sound level)
+  Leq = 20*log10(p_RMS/pRef)
+  
+  return(Leq)
+}
+
+#####  psdLeq
+psd2leq <- function (psd, gain, sensitivity=-35, bit=16, Vadc=2, pRef=20e-6) 
+{  
+  # convert P (amplitude²) to pressure²
+  P = wav2pressure (sqrt(psd), gain, sensitivity, bit, Vadc)**2
+  
+  # if P ==0 set to MIN
+  P[P==0] = MIN
+  
+  # Energy (pressure^2) to Leq 
+  # => Leq (Equivalent Continuous Sound level) if the sum is performed on the whole PSD
+  leq = 10*log10(sum(P)/pRef**2)
+  return (leq)
+}
+
+########################
 
 #******** Convert a full PSD into bins PSD with a resolution = res
 specbin <- function (spec, f, res = 0.5)
@@ -56,7 +212,7 @@ specbin <- function (spec, f, res = 0.5)
   return (list(f = freq_bin, s = spec_bin))
 }
 
-
+########################
 
 #******** geometrical (or spherical) attenuation
 propa.Ageo <- function(r,r0) 
@@ -140,6 +296,8 @@ propa.Aatm <- function(f, r=NA, r0=NA, t=20, rh=60, pa=101325)
   return (list(factor = Aatm, db = Aatm.dB, coef = coef.Aatm, coef.db = coef.Aatm.dB))
 }
 
+########################
+
 #********  Habitat attenuation
 propa.Ahab<- function(f, r=NA, r0=NA, A0=0.02)
 {
@@ -181,6 +339,7 @@ propa.Ahab<- function(f, r=NA, r0=NA, A0=0.02)
   return (list(factor = Ahab, db = Ahab.dB, coef = coef.Ahab, coef.db = coef.Ahab.dB))
 }
 
+########################
 
 #************ full attenuation
 propa.Atotal  <- function(f, r, r0, t=20, rh=60, pa=101325, A0=0.02)
@@ -215,6 +374,8 @@ propa.Atotal  <- function(f, r, r0, t=20, rh=60, pa=101325, A0=0.02)
   return (list(factor = Atotal, db = Atotal.dB))
 }
 
+########################
+
 #************** Repartition of energy along the frequencies
 propa.dBSPL_per_bin <- function(L, f)
 {
@@ -248,6 +409,8 @@ propa.dBSPL_per_bin <- function(L, f)
   
   return (list(f=vector[,1], db=vector[,2]))
 }
+
+########################
 
 #************** detection distance
 propa.detection_distance  <- function(L_bkg, L0, f, r0= 1, delta_r=1, t=20, rh=60, pa=101325, A0=0.02)
@@ -299,6 +462,8 @@ propa.detection_distance  <- function(L_bkg, L0, f, r0= 1, delta_r=1, t=20, rh=6
   return (cbind(f,distance_max))
 }
 
+########################
+
 # #************* apply attenuation
 propa.apply.att <- function(p0, fs, r, r0= 1, t=20, rh=60, pa=101325, A0=0.02)
 {
@@ -333,6 +498,7 @@ propa.apply.att <- function(p0, fs, r, r0= 1, t=20, rh=60, pa=101325, A0=0.02)
   return (p)
 }
 
+########################
 
 #************************** Get the pressure at r0 from simulation
 propa.p_at_r0 <- function(f, r, p, r0=1, t=20, rh=60, pa=101325, A0=0.02)
